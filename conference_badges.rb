6adbec1134171263cf46b1f2dc0fd7620db94aae
Mark Drehmann <markdrehmann@gmail.com>
2020-01-18 14:56:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,3 +9,11 @@ def batch_badge_creator(array)
     badge_maker(badge)
   end
 end
+
+def assign_rooms(array)
+  counter = 1
+  array.collect do |name|
+    "Hello, #{name}! You'll be assigned to room #{counter}!"
+    counter += 1
+  end
+end
